58a0b26fe283fef254449ce27d7cf06f0dca4753
DrGlas1 <elliot.glas@hotmail.se>
2023-11-29 17:11:20 +0100

    able to test intopt, mem fault atm

--- intopt.c
+++ intopt.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-#include "simplex.h"
 #define EPSILON 1e-6
+
+typedef struct simplex_t{
+	int m, n; 			
+	int* var;
+	double** a;		 
+	double* b;		
+	double* c;
+	double* x;
+	double y;		
+} simplex_t;
 
 typedef struct node_t {
 	int m, n, k, h;
@@ -21,6 +30,9 @@ typedef struct queue {
 	struct queue* succ;
 	node_t* node;
 } queue;
+
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h);
+
 
 void free_node(node_t* node) {
 	free(node->min);
@@ -122,7 +134,273 @@ void print_queue(queue* h) {
 	printf("\n");
 }
 
- 
+void print(simplex_t* s) {
+	double** a = s->a;
+	double* b = s->b;
+	double* c = s->c;
+	double* x = s->x;
+	int* var = s->var;
+	int m = s->m;
+	int n = s->n;
+	int i, j;
+	printf("-----------------------------------------------\n");
+	printf("maximize: ");
+	for(i = 0; i < n; i++) {
+		printf("%.2fx_%d ", c[i],var[i]);
+		printf("+ ");
+	}
+	
+	printf("%.2f\n", s->y);
+	printf("\nsubject to: \n");
+	for(i = 0; i < m; i++) {
+		printf("x_%d = %.2f -(", var[i+n], b[i]);
+		for(j = 0; j < n; j++) {
+			printf("%.2fx_%d", a[i][j], var[j]);
+			if (j != n - 1) {
+				printf(" + ");
+			}
+		}
+		printf(")\n");
+	}
+	printf("\n");
+}
+
+
+
+int init(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var) {
+	int i;
+	int k = 0;
+	s->m = m;
+    	s->n = n;
+    	s->var = var;
+    	s->a = a;
+    	s->b = b;
+    	s->c = c;
+    	s->x = x;
+    	s->y = y;
+	if (s->var == NULL) {	
+		s->var = malloc((m+n+1) * sizeof(int));
+		for(i = 0; i < m + n; i++) {
+			s->var[i] = i;
+		}
+	}
+	for (i = 1; i < m; i++) {
+		if (b[i] < b[k]) {
+			k = i;
+		}
+	} 
+	return k;
+
+}
+
+void pivot(simplex_t* s, int row, int col) {
+	double** a = s->a;
+	double* b = s->b;
+	double* c = s->c;
+	int m = s->m;
+	int n = s->n;
+	int i,j,t;
+	t = s->var[col];
+	s->var[col] = s->var[n + row];
+	s->var[n + row] = t;
+	s->y += c[col] * b[row] / a[row][col];
+	for(i = 0; i < n; i++) {
+		if (i != col) {
+			c[i] -= c[col] * a[row][i] / a[row][col];
+		}
+	}
+	c[col] = -c[col] / a[row][col];
+	for(i = 0; i < m; i++) {
+		if (i != row) {
+			b[i] -= a[i][col] * b[row] / a[row][col];
+		}
+	}
+	for(i = 0; i < m; i++) {
+		if (i != row) {
+			for(j = 0; j < n; j++) {
+				if (j != col) {
+					a[i][j] -= a[i][col] * a[row][j] / a[row][col];
+				}
+			}
+		}
+	}
+	for(i = 0; i < m; i++) {
+		if (i != row) {
+			a[i][col] = -a[i][col] / a[row][col];
+		}
+	}
+	for(i = 0; i < n; i++) {
+		if (i != col) {
+			a[row][i] = a[row][i] / a[row][col];
+		}
+	}
+	b[row] = b[row] / a[row][col];
+	a[row][col] = 1 / a[row][col];
+	printf("pivot row=%d col=%d\n", row, col);
+	print(s);
+}
+
+void prepare(simplex_t* s, int k) {
+	int m = s->m;
+	int n = s->n;
+	int i;
+	for(i = m + n; i > n; i--) {
+		s->var[i] = s->var[i - 1];
+	}
+	s->var[n] = m + n;
+	n = n+1;
+	for(i = 0; i < m; i++) {
+		s->a[i][n - 1] = -1;
+	}
+	s->x = malloc((m+n) * sizeof(double));
+	s->c = malloc(n * sizeof(double));
+	s->c[n-1] = -1;
+	s->n = n;
+	pivot(s, k, n - 1);
+}
+
+int select_nonbasic(simplex_t* s) {
+	for(int i = 0; i < s->n; i++) {
+		if (s->c[i] > EPSILON) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+
+bool initial(simplex_t* s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var) {
+	int i, j, k;
+	double w;
+	k = init(s, m, n, a, b, c, x, y, var);
+	if (b[k] >= 0) {
+		return true;
+	}
+	printf("b[%d]=%lf\n", k, b[k]);
+	print(s);
+	prepare(s,k);
+	n = s->n;
+	s->y = xsimplex(m, n, s->a, s->b, s->c, s->x, 0, s->var, 1);
+	for(i = 0; i < m + n; i++) {
+		if(s->var[i] == m+n-1) {
+			if(fabs(s->x[i]) > EPSILON) {
+				free(s->x);
+				free(s->c);
+				return false;
+			} else {
+				break;
+			
+			}
+		}
+	}		
+	if (i >= n) {
+		j = 0;
+		for(k = 0; k < n; k++) {
+			if(fabs(s->a[i-n][k]) > fabs(s->a[i-n][j])) {
+				j = k;
+			}
+		}
+		pivot(s, i-n, j);
+		i = j;	
+	}
+       	if (i < n - 1) {
+		k = s->var[i];
+		s->var[i] = s->var[n-1];
+		s->var[n-1] = k;
+		for(k = 0; k < m; k++) {
+			w = s->a[k][n-1];
+			s->a[k][n-1] = s->a[k][i];
+			s->a[k][i] = w;
+		}
+	}
+	free(s->c);
+	s->c = c;
+	s->y = y;
+	for(k = n-1; k < n + m - 1; k++) {
+		s->var[k] = s->var[k+1];
+	}
+	n = s->n-1;
+	s->n = s->n -1;
+	//Look at this
+	double t[n];
+        for(k = 0; k < n; k++) {
+		for(j = 0; j < n; j++) {
+			if(k == s->var[j]) {
+				t[j] = t[j] + s->c[k];
+				goto next_k;
+			}
+		}
+		for(j = 0; j < m; j++) {
+			if(s->var[n+j] == k) {
+				break;
+			}	
+		}
+		s->y += s->c[k] * s->b[j];
+		for(i = 0; i < n; i++) {
+			t[i] -= s->c[k] * s->a[j][i];
+		}
+		next_k:;
+	}
+	for(i = 0; i < n; i++) {
+		s->c[i] = t[i];
+	}
+	free(s->x);
+	return true;
+}
+
+
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h) {
+	simplex_t* s = malloc(sizeof(simplex_t));
+	int i, row, col;
+	if (!initial(s, m, n, a, b, c, x, y, var)) {
+		free(s->var);
+		return NAN;
+	}
+	print(s);
+	while((col = select_nonbasic(s)) >= 0) {
+		row = -1;
+		for(i = 0; i < m; i++) {
+			if (a[i][col] > EPSILON && (row < 0 || b[i] / a[i][col] < b[row]/a[row][col])) {
+				row = i;
+			}
+		}
+		if (row < 0) {
+			free(s->var);
+			return INFINITY;
+		}
+		pivot(s, row, col);
+	}
+	if (h == 0) {
+		for(i = 0; i < n; i++) {
+			if (s->var[i] < n) {
+				x[s->var[i]] = 0;
+			}
+		}
+		for(i = 0; i < m; i++) {
+			if (s->var[n + i] < n) {
+				x[s->var[n + i]] = s->b[i];
+			}
+		}
+		free(s->var);
+	} else {
+		for(i = 0; i < n; i++) {
+			x[i] = 0;
+		}
+		for(i = n; i < n+m; i++) {
+			x[i] = s->b[i-n];
+		}
+	}
+	double res = s->y;
+	free(s);
+	return res;
+}
+
+double simplex(int m, int n, double** a, double* b, double* c, double* x, double y) {
+	return xsimplex(m, n, a, b, c, x, y, NULL, 0);
+}
+
+
 node_t* initial_node(int m, int n, double** a, double* b, double* c) {
 	node_t* p = (node_t*)malloc(sizeof(node_t));
 	p->a = (double**)malloc((m+1) * sizeof(double));
@@ -319,4 +597,72 @@ double intopt(int m, int n, double** a, double* b, double* c, double* x) {
 	}
 	if (z == -INFINITY) return NAN;
 	return z;
+}
+
+int main() {
+	int m, n;
+
+	scanf("%d %d", &m, &n);
+
+	printf("%d, %d\n", m, n);
+
+	double** a = (double**)malloc(sizeof(double) * m);
+	double* b = (double*)malloc(sizeof(double) * m);
+	double* c = (double*)malloc(sizeof(double) * n);
+	double* x =(double*)malloc(sizeof(double) * (n+1)); 
+	
+	int i, j;
+	
+	for(i = 0; i < m; i++) {
+		a[i] = (double*)malloc(sizeof(double) * (n+1));
+	}
+		
+	for(i = 0; i < n; i++) {
+		scanf("%lf", &c[i]);
+	}
+
+	for(i = 0; i < m; i++) {
+		for(j = 0; j < n; j++) {
+			scanf("%lf", &a[i][j]);
+		}
+	}
+
+	for(i = 0; i < m; i++) {
+		scanf("%lf", &b[i]);
+	}
+
+	printf("max z = ");
+	for(i = 0; i < n; i++) {
+		printf("%.2fx_%d ", c[i], i);
+		if (i != n - 1) {
+			printf("+ ");
+		}
+	}
+	printf("\n");
+
+	for(i = 0; i < m; i++) {
+		for(j = 0; j < n; j++) {
+			printf("%.2fx_%d ", a[i][j], i);
+			if (j != n - 1) {
+				printf("+ ");
+			} else {
+				printf("\u2264 %lf", b[i]);
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+	double res = 0;
+	res = intopt(m, n, a, b, c, x); 
+	printf("%lf\n", res);
+	
+	for(i = 0; i < m; i++) {
+		free(a[i]);
+	}
+
+	free(a);
+	free(b);
+	free(c);
+	free(x);
+	return 0;
 }
--- simplex.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLEX_H
-#define SIMPLEX_H
-
-double simplex(int m, int n, double** a, double* b, double* c, double* x, double y);
-
-#endif
-
-
